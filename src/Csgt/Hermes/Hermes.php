d08382ec0cfe44bf544134b4d07a4d6ef0a79a48
Jose Galindo (Mac) <jgalindo@cs.com.gt>
2015-05-22 08:56:16 -0600

    Cambiar el nombre del sender

--- src/Csgt/Hermes/Hermes.php
+++ src/Csgt/Hermes/Hermes.php
@@ -116,7 +116,7 @@ class Hermes {
 
 			Mail::send(Config::get('hermes::notificacionview'), $parametros, function($message) use ($codigo) {
 				$message->from(Config::get('hermes::notificacionemail'), Config::get('hermes::notificacionfrom').' | '.Config::get('hermes::notificaciontitulo'));
-	     	$message->subject('Error ' . $codigo);
+	     	$message->subject(Config::get('hermes::notificaciontitulo') . ' - Error ' . $codigo);
 	     	$message->to(Config::get('hermes::notificarerrores'));
 			});
 		} catch (Exception $e) { }
